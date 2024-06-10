18e2aa1e57078dde79ba1670985631b6b4bbaa33
Shrey Gupta <gupta.shrey@outlook.com>
2024-06-10 22:32:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/about-me/'  // Adjust this path to match your repository name
+  base: '/about-me/'  // Set the base path to your repository name
 });
