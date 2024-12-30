a1cace5ecda0095be4e42da1f14a615a3b6b15d1
Shrey Gupta <gupta.shrey@outlook.com>
2024-12-31 01:19:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure this matches your deployment base path
   optimizeDeps: {
     include: [
       'react-tooltip',
